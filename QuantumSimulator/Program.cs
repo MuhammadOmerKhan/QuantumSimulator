3aac31e4b64b8864913a0232e7ef9a27c5c5f503
MuhammadOmerKhan <omer.khan@danatonline.com>
2023-01-31 17:25:24 +0500

    Add project files.

---
+++ QuantumSimulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace QuantumTeleportation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var qsim = new QuantumSimulator();
+            var initialStates = new[] { Result.Zero, Result.One };
+
+            foreach (var initialState in initialStates)
+            {
+                var result = await Teleport.Run(qsim, initialState);
+                Console.WriteLine($"Initial state: {initialState} => Teleported state: {result}");
+            }
+        }
+    }
+}
---
+++ QuantumSimulator/QuantumTeleportation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace QuantumTeleportation
+{
+    class Teleport
+    {
+        public static Task<Result> Run(QuantumSimulator qsim, Result initial)
+        {
+            // Perform your teleportation algorithm here
+            return Task.FromResult(initial);
+        }
+    }
+}
